8d67e2ef15016bcbe8afa0ba4c68f0cd51f83ba9
markadriii <69063424+markadriii@users.noreply.github.com>
2024-09-24 20:47:22 +0800

    chore: replace font ttf files to woff2
    - update static font files
    - update @font-face
    - add font preload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,42 @@
 
 <svelte:head>
 	<title>{data.metadata.title}</title>
+
+	<link
+		rel="preload"
+		href="/fonts/inter/light.woff2"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		href="/fonts/inter/regular.woff2"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		href="/fonts/inter/medium.woff2"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		href="/fonts/inter/semibold.woff2"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		href="/fonts/inter/bold.woff2"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
 </svelte:head>
 
 <slot></slot>
