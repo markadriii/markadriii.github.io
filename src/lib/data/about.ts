13a2410bf82e8e19935294e2bc64ad90b05beca0
markadriii <69063424+markadriii@users.noreply.github.com>
2024-10-19 00:45:35 +0800

    chore: update data

--- src/lib/data/about.ts
+++ src/lib/data/about.ts
@@ -1,11 +1,14 @@
 import type { TParagraph } from '$lib/types';
+import { getUrl } from '$lib/utils';
 
 export const about: TParagraph[] = [
 	[
-		"Hi there, I'm Adrian! I'm enthusiastic about utilizing my skills and technical expertise to contribute to projects. I am dedicated to continuous growth through making contributions while building meaningful connections in the tech industry."
+		"Hi, I'm ",
+		`[Mark Adrian](${getUrl()})`,
+		"! I'm enthusiastic about utilizing my skills and technical expertise to contribute to projects. I am eager to learn and grow, embracing new challenges while building meaningful connections in the tech industry."
 	],
 	[
-		'I earned (joke wala pa pls lang baka naman) a Bachelor of Science in Computer Science, major in Software Technology, from ',
+		'I graduated with a Bachelor of Science in Computer Science, majoring in Software Technology, from ',
 		'[De La Salle University - Manila](https://www.dlsu.edu.ph/)',
 		' in 2024. Prior to that, I completed the STEM track during my Senior High School years at ',
 		'[Darwin International School](https://darwininternationalschool.org.ph/)',
--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,11 +1,12 @@
 import type { TContentRaw } from '$lib/types';
+import { getUrl } from '$lib/utils';
 
 export const projects: TContentRaw[] = [
 	{
 		src: '/assets/content/dev_portfolio_logo.png',
 		alt: 'Developer Portfolio Logo',
 		title: 'Developer Portfolio',
-		url: 'https://github.com/markadriii/markadriii.github.io',
+		url: getUrl(),
 		subtitle: 'Personal Project',
 		date: 'Sep 2024',
 		info: 'A clear and informative portfolio website highlighting my development skills, professional growth, and notable projects.',
